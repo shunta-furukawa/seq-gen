b3cd48310912ffd3d369968700b690122637d41c
abema-shunta <furukawa_shunta@abema.tv>
2025-01-13 10:20:32 +0900

    initialize grpc server protobuf

---
+++ test/grpcserver/server.go
@@ -0,0 +1,36 @@
+package grpcserver
+
+import (
+	"context"
+	"net"
+
+	pb "/test/grpcserver/proto"
+
+	"google.golang.org/grpc"
+)
+
+// SimpleServer is a test implementation of the gRPC server.
+type SimpleServer struct {
+	pb.UnimplementedTestServiceServer
+}
+
+func (s *SimpleServer) TestMethod(ctx context.Context, req *pb.TestRequest) (*pb.TestResponse, error) {
+	return &pb.TestResponse{Message: "Hello " + req.Name}, nil
+}
+
+func StartGRPCServer() (*grpc.Server, net.Listener, error) {
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterTestServiceServer(grpcServer, &SimpleServer{})
+	go func() {
+		if err := grpcServer.Serve(listener); err != nil {
+			panic(err)
+		}
+	}()
+
+	return grpcServer, listener, nil
+}
